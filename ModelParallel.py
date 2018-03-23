494751d6262327f66f566160e331049593844ffd
waitwaitforget <tiank311@gmail.com>
2018-03-23 17:41:34 -0400

    update

--- ModelParallel.py
+++ ModelParallel.py
@@ -1,11 +1,57 @@
 import torch
 import torch.nn as nn
-from torch.nn.parallel.parallel_apply import parallel_apply
-from torch.nn.parallel.scatter_gather import scatter_kwargs, gather
+import threading
 
 
 def replicate(module, device):
     return module.cuda(device)
+
+
+def parallel_apply(modules, inputs, kwargs_tup=None, devices=None):
+    assert len(modules) == len(inputs)
+    if kwargs_tup is not None:
+        assert len(kwargs_tup) == len(modules)
+    else:
+        kwargs_tup = ({},) * len(modules)
+    if devices is not None:
+        assert len(modules) == len(devices)
+    else:
+        raise VauleError('devices is None')
+
+    lock = threading.Lock()
+    results = {}
+    grad_enabled = torch.is_grad_enabled()
+
+    def _worker(i, module, input, kwargs, device=None):
+        torch.set_grad_enabled(grad_enabled)
+        try:
+            with torch.cuda.device(device):
+                output = module(*input, **kwargs)
+            with lock:
+                results[i] = output
+        except Exception as e:
+            with lock:
+                results[i] = e
+
+    if len(modules) > 1:
+        threads = [threading.Thread(target=_worker,
+                                    args=(i, module, input, kwargs, device))
+                   for i, (module, input, kwargs, device) in
+                   enumerate(zip(modules, inputs, kwargs_tup, devices))]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+    else:
+        _worker(0, modules[0], inputs[0], kwargs_tup[0], devices[0])
+
+    outputs = []
+    for i in range(len(inputs)):
+        output = results[i]
+        if isinstance(output, Exception):
+            raise output
+        output.append(output)
+    return outputs
 
 
 class ModelParallel(nn.Module):
@@ -31,32 +77,36 @@ class ModelParallel(nn.Module):
             output_device = device_ids[0]
         self.device_ids = device_ids
         self.module = model.module  # module is a list
+        self.replicate(self.module, device_ids)
 
     def forward(self, *inputs, **kwargs):
         if not self.device_ids:
             return self.module(*inputs, **kwargs)
-        inputs, kwargs = self.scatter(input, kwargs, self.device_ids)
+        inputs, kwargs = self.scatter(inputs, kwargs, self.device_ids)
         if len(self.device_ids) == 1:
             return self.module(*inputs[0], **kwargs[0])
-        replicas = self.replicate(self.module, self.device_ids)
-        outputs = self.parallel_apply(replicas, inputs, kwargs)
+
+        outputs = self.parallel_apply(self.module, inputs, kwargs)
         return self.gather(outputs, self.output_device)
 
     def replicate(self, module, device_ids):
         return [replicate(m, id) for m, id in zip(module, device_ids)]
 
     def scatter(self, inputs, kwargs, device_ids):
-        inputs = [input.cuda(id) for input, id in zip(inputs, device_ids)]
-        kwargs = kwargs * device_ids
+        if len(inputs) == 1:
+            inputs = [inputs[0].cuda(id) for id in device_ids]
+        else:
+            inputs = [input.cuda(id) for input, id in zip(inputs, device_ids)]
+        kwargs = None
         inputs = tuple(inputs)
-        kwargs = tuple(kwargs)
         return inputs, kwargs
 
     def parallel_apply(self, replicas, inputs, kwargs):
         return parallel_apply(replicas, inputs, kwargs, self.device_ids)
 
     def gather(self, outputs, output_device):
-        return gather(outputs, output_device)
+        outputs = [output.cuda(output_device) for output in outputs]
+        return outputs
 
 
 def unittest():
--- test.py
+++ test.py
@@ -8,7 +8,12 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.linear = nn.Linear(3, 10)
+        self.linear = nn.Sequential(nn.Linear(3, 100),
+                                    nn.Sigmoid(),
+                                    nn.Linear(100, 300),
+                                    nn.Sigmoid(),
+                                    nn.Linear(300, 2)
+                                    )
 
     def forward(self, input):
         return self.linear(input)
@@ -24,9 +29,19 @@ class Ensemble(nn.Module):
         return [module(input) for module in self.module]
 
 
-model = Ensemble(3)
+ensemble = Ensemble(5)
 
-model = ModelParallel(model, device_ids=[0, 1, 2], output_device=0)
-print model
+model = ModelParallel(ensemble, device_ids=[0, 1, 2, 4, 5], output_device=0)
+print len(model.module.module)
 input = Variable(torch.rand(10, 3))
-print model(input)
+
+import time
+end = time.time()
+y = model(input)
+print('using model parallel')
+print('time : ', time.time() - end)
+
+end = time.time()
+y = ensemble(input)
+print('without model parallel')
+print('time: ', time.time() - end)
