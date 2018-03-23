21bcceaddfc00babab203b391bc6e0ac2f50eca9
waitwaitforget <tiank311@gmail.com>
2018-03-23 19:21:11 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# modelparallel_pytorch
+# Model Parallelism
 Model Parallelism for pytorch training multiple network on multiple GPUs.
+
+# Usage
+Model parallel is a wrapper for multiple networks.
+
+Unlike data parallel, the outputs of model parallel is a list for general purpose.
+
+```python
+# First define a ensemble module
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from ModelParallel import ModelParallel
+
+
+class Ensemble(nn.Module):
+    def __init__(self, m):
+        super(Ensemble, self).__init__()
+        self.m = m
+        self.module = nn.ModuleList([models.resnet50() for _ in range(m)])
+
+    def forward(self, input):
+        return [self.module[i](input) for i in range(self.m)]
+
+model = Ensemble(4)
+model = ModelParallel(model, device_ids=[0, 1, 2, 3], output_device=0)
+
+x = torch.rand(128, 3, 224, 224)
+y = model(Variable(x))
+
+```
+
+## Useful links
+Some multithreading code borrow from [pytorch data parallel](https://github.com/pytorch/pytorch/blob/v0.3.1/torch/nn/parallel/parallel_apply.py)
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import time
 
 class SmallNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(SmallNet, self).__init__()
 
         self.linear = nn.Sequential(nn.Linear(30, 1000),
                                     nn.Sigmoid(),
@@ -25,7 +25,7 @@ class Ensemble(nn.Module):
     def __init__(self, m, mode='small'):
         super(Ensemble, self).__init__()
         if mode == 'small':
-            self.module = nn.ModuleList([Net() for i in range(m)])
+            self.module = nn.ModuleList([SmallNet() for i in range(m)])
         elif mode == 'large':
             self.module = nn.ModuleList([models.resnet50() for i in range(m)])
 
@@ -50,7 +50,12 @@ def test_model_parallel(mode='small'):
 
 def test_without_parallel(mode='small'):
     ensemble = Ensemble(4)
-    [ensemble.moudle[i].cuda(i) for i in range(4)]
+    [ensemble.module[i].cuda(i) for i in range(4)]
+
+    if mode == 'small':
+        input = Variable(torch.rand(512, 30))
+    elif mode == 'large':
+        input = Variable(torch.rand(128, 3, 224, 224))
 
     end = time.time()
     y = [ensemble.module[i](input.cuda(i)) for i in range(4)]
